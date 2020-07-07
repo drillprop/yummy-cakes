11570d0fb0a30bb97697a4e98850e733c4795090
drillprop <drylbartosz@gmail.com>
2020-07-07 16:40:36 +0200

    Create Wrapper and FlexBox styled component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components"
+import styled, { createGlobalStyle, css } from "styled-components"
 import { normalize } from "styled-normalize"
 
 const GlobalStyle = createGlobalStyle`
@@ -21,7 +21,64 @@ const GlobalStyle = createGlobalStyle`
     overscroll-behavior: none;
     overflow-x: hidden;
   }
-
-
 `
+
+export const Wrapper = styled.div<{ fluid?: boolean }>`
+  flex-grow: 1;
+  padding: 32px;
+  margin: 0 auto;
+  position: relative;
+  width: auto;
+  height: 100%;
+  @media (min-width: 1024px) {
+    max-width: 960px;
+  }
+  @media (min-width: 1216px) {
+    max-width: 1152px;
+  }
+  @media (min-width: 1408px) {
+    max-width: 1244px;
+  }
+  ${props =>
+    props.fluid &&
+    css`
+      padding: 0;
+      margin: 0;
+      max-width: 100%;
+    `};
+`
+
+interface FlexBoxProps {
+  spaceBetween?: boolean
+  flexEnd?: boolean
+  alignTop?: boolean
+  noHeight?: boolean
+}
+
+export const FlexBox = styled.div<FlexBoxProps>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  ${props =>
+    props.spaceBetween &&
+    css`
+      justify-content: space-between;
+    `};
+  ${props =>
+    props.flexEnd &&
+    css`
+      justify-content: flex-end;
+    `};
+  ${props =>
+    props.alignTop &&
+    css`
+      align-items: top;
+    `};
+  ${props =>
+    props.noHeight &&
+    css`
+      height: 0;
+    `};
+`
+
 export default GlobalStyle
